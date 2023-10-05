c91ccf48a4e120305672a5e7d83021f128411a0d
nikita venediktov <nikben2105@gmail.com>
2023-10-05 18:18:48 +0300

    Backend's created

---
+++ Back/TestEnv/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestEnv.Controllers
+{
+    [ApiController]
+    [Route("/hello-world")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetHelloWorld()
+        {
+            return this.Ok("Hello world!");
+        }
+    }
+}
